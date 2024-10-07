e831c45497c5a521431f3d82495405f3e604425a
Llily766 <au.binet21@gmail.com>
2024-10-07 13:23:37 +0200

    EPREUVE E5

--- index.php
+++ index.php
@@ -13,8 +13,7 @@
             <label for="username">Nom d'utilisateur</label>
             <input type="text" id="username" name="username" required>
 
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required>
+            
 
             <label for="password">Mot de passe</label>
             <input type="password" id="password" name="password" required>
