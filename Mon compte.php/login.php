2dce79595f6f5a6eaf7f3a753e9a298c37fed185
Llily766 <au.binet21@gmail.com>
2024-11-14 12:54:37 +0100

    epreuve e5

--- Mon compte.php/login.php
+++ Mon compte.php/login.php
@@ -1,5 +1,11 @@
-<html>
-    Se connecter
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet">
+    <title>Document</title>
+</head>
 
     <body>
     <form action="testConnexion.php" method="POST">
